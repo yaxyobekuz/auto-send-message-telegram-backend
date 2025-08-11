22480f6da6b5d93cf6a6e885ea5f77ec3354e877
yaxyobekuz <pubgn9642@gmail.com>
2025-08-11 11:56:08 +0500

    Add required field to phone key in User model

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   token: { type: String },
   session: { type: String },
-  phone: { type: String, unique: true },
   name: { type: String, required: true },
   groupsCount: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
+  phone: { type: String, unique: true, required: true },
   role: { type: String, default: "admin", enum: ["admin", "owner"] },
 });
 

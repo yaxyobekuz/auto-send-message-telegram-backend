d9c41a9c70e2aafdab1ff69fac78303d9bbf9b35
yaxyobekuz <pubgn9642@gmail.com>
2025-08-11 19:46:52 +0500

    Update group model for auto send message

--- src/models/Group.js
+++ src/models/Group.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const groupSchema = new mongoose.Schema({
+  className: { type: String },
   userId: { type: String, required: true },
+  chatId: { type: String, required: true },
+  accessHash: { type: String, default: null },
   createdAt: { type: Date, default: Date.now },
   title: { type: String, default: "Mavjud emas" },
-  chatId: { type: String, unique: true, required: true },
   type: { type: String, default: "group", enum: ["group", "supergroup"] },
 });
 
